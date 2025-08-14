b8a4ef87238024a732bb93a19def701b06b69d00
CHINTAKRINDI LAKSHMI VIVEK <vivekch1225@gmail.com>
2025-08-14 23:34:06 +0530

    Time: 39 ms (78.31%), Space: 61.1 MB (34.26%) - LeetHub

---
+++ 0164-maximum-gap/0164-maximum-gap.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maximumGap(int[] nums) {
+        int max =0;
+        Arrays.sort(nums);
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]-nums[i-1]>max){
+                max = nums[i]-nums[i-1];
+            }
+        }
+        return max;
+    }
+}
