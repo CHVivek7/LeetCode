ba529ef0692cfcaff5f3101ab2b971c6a19f353f
CHINTAKRINDI LAKSHMI VIVEK <vivekch1225@gmail.com>
2025-08-14 23:20:21 +0530

    Time: 1 ms (100%), Space: 45 MB (8.13%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -10,16 +10,59 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        String str="";
-        while(l1!=null){
-            str = str+l1.val;
+        ListNode itr1 = l1;
+        ListNode itr2 = l2;
+        ListNode itr3 = l1;
+        int c = 0;
+        int b=0;
+        while(itr1 != null && itr2 != null){
+            int a = itr1.val + itr2.val;
+            a+=c;
+            c=0;
+            if(a>9){
+                c=a/10;
+                a=a%10;
+            }
+            itr1.val=a;
+            itr2.val=a;
+            itr3=itr1;
+            itr1=itr1.next;
+            itr2=itr2.next;
         }
-        
-        String str1="";
-        while(l2!=null){
-            str1 = str1+l2.val;
+        while(itr1 != null){
+            int a = itr1.val;
+            a+=c;
+            c=0;
+            if(a>9){
+                c=a/10;
+                a=a%10;
+            }
+            itr1.val=a;
+            itr3=itr1;
+            itr1=itr1.next;
         }
-        int num1 = Integer.parseInt(str);
+        while(itr2 != null){
+            int a = itr2.val;
+            a+=c;
+            c=0;
+            if(a>9){
+                c=a/10;
+                a=a%10;
+            }
+            itr2.val=a;
+            itr3=itr2;
+            itr2=itr2.next;
+            b=1;
+        }
+        if(c!=0){
+            ListNode x = new ListNode();
+            x.val=c;
+            itr3.next = x;
+            x.next=null;
+        }
+        if(b==1){
+            return l2;
+        }
         return l1;
     }
 }
