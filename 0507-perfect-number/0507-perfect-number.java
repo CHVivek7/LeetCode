7e93bded3911a51358ee04b3e65ea1638571e70a
CHINTAKRINDI LAKSHMI VIVEK <vivekch1225@gmail.com>
2025-08-14 23:55:02 +0530

    Time: 2114 ms (10.57%), Space: 40.9 MB (11.47%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        List<Integer> list = new ArrayList<>();
+        for(int i =1;i<num;i++){
+            if(num%i ==0){
+                list.add(i);
+            }
+        }
+        int sum =0;
+        for(int i=0;i<list.size();i++){
+            sum+=list.get(i);
+        }
+        if(sum == num){
+            return true;
+        }
+        return false;
+    }
+}
