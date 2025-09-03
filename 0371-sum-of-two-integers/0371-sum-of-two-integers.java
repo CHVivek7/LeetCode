163313d78672109efa5964bfbd8125790276e0df
CHINTAKRINDI LAKSHMI VIVEK <vivekch1225@gmail.com>
2025-09-03 21:11:41 +0530

    Time: 0 ms (100%), Space: 40.2 MB (92.13%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.java
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.java
@@ -1,7 +1,7 @@
 class Solution {
     public int getSum(int a, int b) {
         while (b != 0) {
-            int sum = a ^ b;      // sum bits without carries
+            int sum = a ^ b; 
             int carry = (a & b) << 1; 
             a = sum;
             b = carry;
