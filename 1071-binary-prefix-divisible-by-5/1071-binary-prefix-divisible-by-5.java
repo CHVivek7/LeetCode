c0733bcf392cd498723d349b846da9df97a9ea81
CHINTAKRINDI LAKSHMI VIVEK <vivekch1225@gmail.com>
2025-09-07 12:56:10 +0530

    Time: 3 ms (98.58%), Space: 46.2 MB (12.4%) - LeetHub

--- 1071-binary-prefix-divisible-by-5/1071-binary-prefix-divisible-by-5.java
+++ 1071-binary-prefix-divisible-by-5/1071-binary-prefix-divisible-by-5.java
@@ -2,12 +2,10 @@ class Solution {
     public List<Boolean> prefixesDivBy5(int[] nums) {
         List<Boolean> list = new ArrayList<>();
         int rem = 0; 
-
         for (int bit : nums) {
             rem = ((rem << 1) + bit) % 5;
             list.add(rem == 0);
         }
-
         return list;
     }
 }
