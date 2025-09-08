d9a3af15ddab88bf168c53d9582d6fe208a15970
CHINTAKRINDI LAKSHMI VIVEK <vivekch1225@gmail.com>
2025-09-08 13:34:35 +0530

    Time: 3 ms (44.51%), Space: 44.7 MB (86.58%) - LeetHub

--- 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.java
+++ 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.java
@@ -15,15 +15,9 @@ class Solution {
             list.add(i);
         }
         Arrays.sort(arr);
-        
-        for(int i :arr){
-            System.out.println(i);
-        }
-        System.out.println(list);
         List<Integer> fin = new ArrayList<>();
         for(int i = 0;i<arr.length;i++){
             fin.add(list.indexOf(arr[i]));
-            System.out.println(list.indexOf(arr[i]));
             list.set(list.indexOf(arr[i]), Integer.MAX_VALUE);
             if(fin.size()==k){
                 i=arr.length;
