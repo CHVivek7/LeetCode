f94c4878a84053ff0319c6afd15f399e58d59f52
CHINTAKRINDI LAKSHMI VIVEK <vivekch1225@gmail.com>
2025-09-15 07:09:44 +0530

    Time: 4 ms (27.38%), Space: 42.9 MB (29.7%) - LeetHub

---
+++ 1264-maximum-number-of-words-you-can-type/1264-maximum-number-of-words-you-can-type.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int canBeTypedWords(String text, String brokenLetters) {
+        int count =0;
+        boolean contains = false;
+        String[] str = text.split(" ");
+        for(int i = 0;i<str.length;i++){
+            for(int j = 0;j<brokenLetters.length();j++){
+                if(str[i].contains(brokenLetters.charAt(j)+"")){
+                    contains = true;
+                    j = brokenLetters.length();
+                }
+            }
+            if(!contains){
+                count++;
+            }
+            contains = false;
+        }
+        return count;
+    }
+}
