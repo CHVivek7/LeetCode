ebafd374d74a2115fd817255395b1ed45abc37a1
CHINTAKRINDI LAKSHMI VIVEK <vivekch1225@gmail.com>
2025-10-13 19:17:42 +0530

    Time: 1 ms (99.18%), Space: 42.6 MB (14.73%) - LeetHub

--- 3396-valid-word/3396-valid-word.java
+++ 3396-valid-word/3396-valid-word.java
@@ -1,28 +1,31 @@
 class Solution {
     public boolean isValid(String word) {
-        if (word == null || word.length() < 3) {
+        if(word.length()<3)
+        {
             return false;
         }
-        word = word.toLowerCase();
-        boolean hasVowel = false;
-        boolean hasConsonant = false;
-        String vowels = "aeiouAEIOU";
-
-        for (int i = 0; i < word.length(); i++) {
-            char c = word.charAt(i);
-            if (!Character.isDigit(c) && !(c >= 'a' && c <= 'z')) {
-                System.out.println(word.charAt(i));
+        char arr[]=word.toCharArray();
+        int vow=0,cos=0,dig=0;
+        String vowels="aeiouAEIOU";
+        for(int i=0;i<arr.length;i++)
+        {
+            if(vowels.contains(Character.toString(arr[i])))
+            {
+                vow++;
+            }
+            else if(((arr[i]>=65 && arr[i]<=91) || (arr[i]>=96 && arr[i]<=125)) && !vowels.contains(Character.toString(arr[i])))
+            {
+                cos++;
+            }
+            else if(Character.isDigit(arr[i]))
+            {
+                dig++;
+            }
+            else
+            {
                 return false;
             }
-            if (c >= 'a' && c <= 'z') {
-                if (vowels.indexOf(c) >= 0) {
-                    hasVowel = true;
-                } else {
-                    hasConsonant = true;
-                }
-            }
         }
-
-        return hasVowel && hasConsonant;
+        return (vow>=1 && cos>=1);
     }
 }
