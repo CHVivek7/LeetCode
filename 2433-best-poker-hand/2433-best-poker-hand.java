88a4c0fb45dc5d52aa8a720aa0767f76a623d4bf
CHINTAKRINDI LAKSHMI VIVEK <vivekch1225@gmail.com>
2025-10-14 11:27:05 +0530

    Time: 0 ms (100%), Space: 41.4 MB (33.44%) - LeetHub

--- 2433-best-poker-hand/2433-best-poker-hand.java
+++ 2433-best-poker-hand/2433-best-poker-hand.java
@@ -1,42 +1,18 @@
 class Solution {
     public String bestHand(int[] ranks, char[] suits) {
-        boolean same = true;
-        boolean trice = false;
-        boolean pair = false;
-        int i = 0;
-        Map<Integer, Set<Character>> map = new HashMap<>();
-        for( i = 0;i<ranks.length-1;i++){
-            if(suits[i]!=suits[i+1]){
-                same = false;
-            }
-            Set<Character> set = map.getOrDefault(ranks[i],new HashSet<>());
-            set.add(suits[i]);
-            map.put(ranks[i],set);
-            if(set.size()==3){
-                trice = true;
-            }
-            else if(set.size()==2){
-                pair = true;
-            }
-        }
-        Set<Character> set = map.getOrDefault(ranks[i],new HashSet<>());
-        set.add(suits[i]);
-        map.put(ranks[i],set);
-        System.out.println(map);
-        if(set.size()==3){
-            trice = true;
-        }
-        else if(set.size()==2){
-            pair = true;
-        }
-        if(same){
+        if (suits[0] == suits[1] && suits[1] == suits[2] && suits[2] == suits[3] && suits[3] == suits[4]) {
             return "Flush";
         }
-        else if(trice){
-            return "Three of a Kind";
+        int[] rankCounts = new int[14];
+        for (int rank : ranks) {
+            if (++rankCounts[rank] == 3) {
+                return "Three of a Kind";
+            }
         }
-        else if(pair){
-            return "Pair";
+        for (int count : rankCounts) {
+            if (count == 2) {
+                return "Pair";
+            }
         }
         return "High Card";
     }
