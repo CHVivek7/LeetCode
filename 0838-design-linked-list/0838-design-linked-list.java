2d1a970bace5d068bdb19fd834fdd641a5ca498c
CHINTAKRINDI LAKSHMI VIVEK <vivekch1225@gmail.com>
2025-10-16 18:52:22 +0530

    Time: 6 ms (99.01%), Space: 45.8 MB (29.8%) - LeetHub

---
+++ 0838-design-linked-list/0838-design-linked-list.java
@@ -0,0 +1,37 @@
+class MyLinkedList {
+    public List<Integer> list;
+
+    public MyLinkedList() {
+        list = new ArrayList<>();    
+    }
+    
+    public int get(int index) {
+        if (index >= list.size()) {
+            return -1;
+        }
+        return list.get(index);
+    }
+    
+    public void addAtHead(int val) {
+        list.add(0, val);
+    }
+    
+    public void addAtTail(int val) {
+        list.add(val);
+    }
+    
+    public void addAtIndex(int index, int val) {
+        if (index > list.size()) {
+            return;
+        }
+        list.add(index, val);
+    }
+    
+    public void deleteAtIndex(int index) {
+        if (index >= list.size()) {
+            return;
+        }
+        list.remove(index);
+        
+    }
+}
