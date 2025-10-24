ec0095403ffff9e6c3afc5df7a6265d842ca65a9
CHINTAKRINDI LAKSHMI VIVEK <vivekch1225@gmail.com>
2025-10-24 09:47:06 +0530

    Time: 10 ms (8.31%), Space: 44.5 MB (18.22%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public String simplifyPath(String path) {
+        String[] parts = path.split("/+"); 
+        Stack<String> stack = new Stack<>();
+
+        for (String part : parts) {
+            if (part.equals("") || part.equals(".")) {
+                continue;
+            } else if (part.equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else {
+                stack.push(part);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.insert(0, "/" + stack.pop());
+        }
+
+        return sb.length() > 0 ? sb.toString() : "/";
+    }
+}
