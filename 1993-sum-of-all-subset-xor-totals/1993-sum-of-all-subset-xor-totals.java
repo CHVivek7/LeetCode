ed116d48fdb028824bbbe50bcad519e17e9f2401
CHINTAKRINDI LAKSHMI VIVEK <vivekch1225@gmail.com>
2025-10-31 13:34:10 +0530

    Time: 0 ms (100%), Space: 42.6 MB (13.27%) - LeetHub

--- 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.java
+++ 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.java
@@ -1,31 +1,49 @@
 class Solution {
-    static List<Integer> hset = new ArrayList<>();
-    public static void subset(int[] set, int n, int m, List<Integer> partial, int index) {
-        if (partial.size() == m) {
-            int xor = partial.get(0);
-            for(int i = 1;i<partial.size();i++){
-                xor ^= partial.get(i);
-            }
-            hset.add(xor);
-            return;
-        }
-        if (index >= n) {
-            return;
-        }
-        partial.add(set[index]);
-        subset(set, n, m, partial, index + 1);
-        partial.remove(partial.size() - 1);
-        subset(set, n, m, partial, index + 1);
-    }
+    int xorSum = 0;
     public int subsetXORSum(int[] nums) {
-        hset.clear();
-        for (int i = 1; i <= nums.length; i++) {
-            subset(nums, nums.length, i, new ArrayList<>(), 0);
+        solve(nums,0,0);
+        return xorSum;
+    }
+
+    public void solve(int[] a, int i, int xor){
+        if(i>a.length) return;
+        if(i==a.length){
+            xorSum+=xor;
+            return;
         }
-        int sum = 0;
-        for(int i : hset){
-            sum += i;
-        }
-        return sum;
+
+        solve(a,i+1,xor^a[i]);
+        solve(a,i+1,xor);
     }
 }
+// class Solution {
+//     static List<Integer> hset = new ArrayList<>();
+//     public static void subset(int[] set, int n, int m, List<Integer> partial, int index) {
+//         if (partial.size() == m) {
+//             int xor = partial.get(0);
+//             for(int i = 1;i<partial.size();i++){
+//                 xor ^= partial.get(i);
+//             }
+//             hset.add(xor);
+//             return;
+//         }
+//         if (index >= n) {
+//             return;
+//         }
+//         partial.add(set[index]);
+//         subset(set, n, m, partial, index + 1);
+//         partial.remove(partial.size() - 1);
+//         subset(set, n, m, partial, index + 1);
+//     }
+//     public int subsetXORSum(int[] nums) {
+//         hset.clear();
+//         for (int i = 1; i <= nums.length; i++) {
+//             subset(nums, nums.length, i, new ArrayList<>(), 0);
+//         }
+//         int sum = 0;
+//         for(int i : hset){
+//             sum += i;
+//         }
+//         return sum;
+//     }
+// }
