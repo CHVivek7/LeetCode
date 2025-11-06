168b65dfbda0c4004d80d86c9e405dffb2990376
CHINTAKRINDI LAKSHMI VIVEK <vivekch1225@gmail.com>
2025-11-06 16:42:04 +0530

    Time: 3 ms (40.57%), Space: 43 MB (36.44%) - LeetHub

--- 2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.java
+++ 2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.java
@@ -12,3 +12,32 @@ class Solution {
         return count;
     }
 }
+
+
+// class Solution {
+//     public int timeRequiredToBuy(int[] tickets, int k) {
+//         Queue<List<Integer>> queue = new LinkedList<>();
+// 		for(int i =0;i<tickets.length;i++){
+// 		    List<Integer> list = new ArrayList<>();
+// 		    list.add(i);
+// 		    list.add(tickets[i]);
+// 		    queue.offer(list);
+// 		}
+// 		int count = 0;
+// 		boolean ch = true;
+// 		while(ch){
+// 		    List<Integer> list = queue.poll();
+// 		    if(list.get(1) != 0){
+// 		        list.set(1, list.get(1)-1);
+// 		        if(list.get(1) !=0){
+// 		            queue.offer(list);
+// 		        }
+// 		        count++;
+// 		    }
+// 		    if(list.get(0) == k && list.get(1)==0){
+// 		        ch = false;
+// 		    }
+// 		}
+// 		return count;
+//     }
+// }
