0cf8a6026b6f98132d5c30f1f5bb3c2f261ea6eb
CHINTAKRINDI LAKSHMI VIVEK <vivekch1225@gmail.com>
2025-11-14 10:51:40 +0530

    Time: 4 ms (36.53%), Space: 80.4 MB (9.84%) - LeetHub

---
+++ 2634-minimum-common-value/2634-minimum-common-value.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution {
+    public int getCommon(int[] nums1, int[] nums2) {
+        for (int i = 0; i < nums1.length; i++) {
+            int idx = Arrays.binarySearch(nums2, nums1[i]);
+            if (idx >= 0) {
+                return nums1[i];
+            }
+        }
+        return -1;
+    }
+}
